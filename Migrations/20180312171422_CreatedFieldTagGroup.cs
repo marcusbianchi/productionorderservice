a51c23a88a6f0df9cf6cdaf495794e1d292aec08
Fernando Souza Silva <fernandos@spi.local>
2018-03-12 14:20:24 -0300

    Create migrations

---
+++ Migrations/20180312171422_CreatedFieldTagGroup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace productionorderservice.Migrations
+{
+    public partial class CreatedFieldTagGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "tagGroup",
+                table: "Tags",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tagGroup",
+                table: "Tags");
+        }
+    }
+}
