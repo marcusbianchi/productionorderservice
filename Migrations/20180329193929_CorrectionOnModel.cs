d54ea8a73cdf5207e0e5251acd67b3ff9fab8618
Marcus <marcusv.bianchisantos@gmail.com>
2018-03-29 16:40:03 -0300

    Correction on Model.

---
+++ Migrations/20180329193929_CorrectionOnModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace productionorderservice.Migrations
+{
+    public partial class CorrectionOnModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "maxValue",
+                table: "PhaseParameters");
+
+            migrationBuilder.DropColumn(
+                name: "minValue",
+                table: "PhaseParameters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "measurementUnit",
+                table: "PhaseParameters",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "measurementUnit",
+                table: "PhaseParameters",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "maxValue",
+                table: "PhaseParameters",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "minValue",
+                table: "PhaseParameters",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
--- Model/PhaseParameter.cs
+++ Model/PhaseParameter.cs
@@ -2,23 +2,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
-namespace productionorderservice.Model
-{
-    public class PhaseParameter
-    {
+namespace productionorderservice.Model {
+    public class PhaseParameter {
         [Key]
         [JsonIgnore]
         public int internalId { get; set; }
+
         [Required]
-        [MaxLength(50)]
+        [MaxLength (50)]
         public string setupValue { get; set; }
-        [Required]
-        [MaxLength(50)]
+
+        [MaxLength (50)]
         public string measurementUnit { get; set; }
-        [MaxLength(50)]
-        public string minValue { get; set; }
-        [MaxLength(50)]
-        public string maxValue { get; set; }
         public Tag tag { get; set; }
     }
 }
