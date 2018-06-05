f95907e7165c1f746063e00460e49880a5a3173c
fernandat <fefedel9@hotmail.com>
2018-06-05 10:15:15 -0300

    security version updated-2

--- Startup.cs
+++ Startup.cs
@@ -17,76 +17,71 @@ using Newtonsoft.Json;
 using productionorderservice.Data;
 using productionorderservice.Services;
 using productionorderservice.Services.Interfaces;
+using securityfilter.Services;
+using securityfilter.Services.Interfaces;
 
-namespace productionorderservice
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace productionorderservice {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-               {
-                   builder.AllowAnyOrigin()
-                          .AllowAnyMethod()
-                          .AllowAnyHeader();
-               }));
-               
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
+                builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ();
+            }));
+
+            services.AddSingleton<IConfiguration> (Configuration);
+            services.AddTransient<IEncryptService, EncryptService> ();
+
             if (!String.IsNullOrEmpty (Configuration["KeyFolder"]))
                 services.AddDataProtection ()
                 .SetApplicationName ("Lorien")
                 .PersistKeysToFileSystem (new DirectoryInfo (Configuration["KeyFolder"]));
-            services.AddSingleton<IRecipePhaseService, RecipePhaseService>();
-            services.AddSingleton<IRecipeService, RecipeService>();
-            services.AddSingleton<IThingService, ThingService>();
-            services.AddSingleton<IThingGroupService, ThingGroupService>();
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("ProductionOrderDb")));
-            services.AddTransient<IStateConfigurationService, StateConfigurationService>();
-            services.AddTransient<IProductionOrderTypeService, ProductionOrderTypeService>();
-            services.AddTransient<IProductionOrderService, ProductionOrderService>();
-            services.AddTransient<IStateManagementService, StateManagementService>();
-            services.AddTransient<IHistStateService,HistStatesService>();
 
-            services.AddTransient<IAssociateProductionOrderService, AssociateProductionOrderService>();
+            services.AddSingleton<IRecipePhaseService, RecipePhaseService> ();
+            services.AddSingleton<IRecipeService, RecipeService> ();
+            services.AddSingleton<IThingService, ThingService> ();
+            services.AddSingleton<IThingGroupService, ThingGroupService> ();
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseNpgsql (Configuration.GetConnectionString ("ProductionOrderDb")));
+            services.AddTransient<IStateConfigurationService, StateConfigurationService> ();
+            services.AddTransient<IProductionOrderTypeService, ProductionOrderTypeService> ();
+            services.AddTransient<IProductionOrderService, ProductionOrderService> ();
+            services.AddTransient<IStateManagementService, StateManagementService> ();
+            services.AddTransient<IHistStateService, HistStatesService> ();
 
-            services.AddResponseCaching();
-            services.AddMvc((options) =>
-            {
-                options.CacheProfiles.Add("productionordercache", new CacheProfile()
-                {
-                    Duration = Convert.ToInt32(Configuration["CacheDuration"]),
-                    Location = ResponseCacheLocation.Any
+            services.AddTransient<IAssociateProductionOrderService, AssociateProductionOrderService> ();
+
+            services.AddResponseCaching ();
+            services.AddMvc ((options) => {
+                options.CacheProfiles.Add ("productionordercache", new CacheProfile () {
+                    Duration = Convert.ToInt32 (Configuration["CacheDuration"]),
+                        Location = ResponseCacheLocation.Any
                 });
-            }).AddJsonOptions(options =>
-            {
+            }).AddJsonOptions (options => {
                 options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-             app.UseResponseCaching();
-            app.UseCors("CorsPolicy");
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseResponseCaching ();
+            app.UseCors ("CorsPolicy");
             app.UseForwardedHeaders (new ForwardedHeadersOptions {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
